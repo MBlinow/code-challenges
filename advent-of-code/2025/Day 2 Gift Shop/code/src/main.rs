9d763177594a8daa6be5b346e3bde4e53ffca80f
MattBlinow <matt.blinow@bitcoindepot.com>
2026-01-01 22:04:16 -0500

    Reading text file, need to process data

--- advent-of-code/2025/Day 2 Gift Shop/code/src/main.rs
+++ advent-of-code/2025/Day 2 Gift Shop/code/src/main.rs
@@ -1,21 +1,45 @@
 // https://adventofcode.com/2025/day/2
 
-use std::{fs, path::Path};
+use std::io::Error;
+use std::{fs, env};
 
 fn main() {
     println!("Hello, world!");
 
-    let file_contents = get_input();
+    let file_contents: String = get_input().unwrap();
 
     println!("file_contents: {file_contents}")
 }
 
-fn get_input()-> String {
-    //let file_path = "/input";
-    let path: &Path = Path::new("./input");
+// fn get_ranges()-> Result<Vec<Range>, Error>{
+//     let file_contents: String = get_input().unwrap();
 
-    let contents: String = fs::read_to_string(path)    
-        .expect("Should have been able to read the file");
+//     let raw_ranges: Vec<String> = file_contents.split(",").collect::<Vec<String>>();
+
+//     println!("Collected ranges are {raw_ranges}");
+
+
+//     let results: Vec<Range> = Vec::with_capacity(raw_ranges.count());
+//     for i in raw_ranges.iter_mut(){
+//         values = i.split('-');
+//         //results.push({start: values[0] end: values[1]});
+//     }
+
+// }
+
+fn get_input()-> Result<String, Error>{
+    let cur_path = env::current_dir()?;
+    println!("current directory is {}", cur_path.display());
+
+    let file_path = cur_path.join("input.txt");
+    println!("reading file at {}", file_path.display());
+
+    let contents = fs::read_to_string(&file_path)?;
     
-    return contents;
+    Ok(contents)
+}
+
+struct Range {
+    start: i32,
+    end: i32
 }
