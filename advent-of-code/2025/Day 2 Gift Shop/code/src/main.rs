eeb45549422bb216bafd365c6e81957e582774a1
MattBlinow <mblinow@gmail.com>
2026-01-03 10:45:02 -0500

    Iteration through ranges setup

--- advent-of-code/2025/Day 2 Gift Shop/code/src/main.rs
+++ advent-of-code/2025/Day 2 Gift Shop/code/src/main.rs
@@ -1,31 +1,37 @@
 // https://adventofcode.com/2025/day/2
 
 use std::io::Error;
-use std::{fs, env};
+use std::{env, fs, result};
 
 fn main() {
     println!("Hello, world!");
 
-    let file_contents: String = get_input().unwrap();
-
-    println!("file_contents: {file_contents}")
+    let ranges = get_ranges();
 }
 
-// fn get_ranges()-> Result<Vec<Range>, Error>{
-//     let file_contents: String = get_input().unwrap();
+fn get_ranges()-> Vec<Range> {
+    let file_contents: String = get_input().unwrap();
 
-//     let raw_ranges: Vec<String> = file_contents.split(",").collect::<Vec<String>>();
+    let raw_ranges: Vec<String> = file_contents
+    .split(",")
+    .map(|s| s.to_string())
+    .collect::<Vec<String>>();
 
-//     println!("Collected ranges are {raw_ranges}");
+    let mut results: Vec<Range> = Vec::with_capacity(raw_ranges.len());
 
+    for i in raw_ranges.iter(){
+        let values = i.split('-')
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
 
-//     let results: Vec<Range> = Vec::with_capacity(raw_ranges.count());
-//     for i in raw_ranges.iter_mut(){
-//         values = i.split('-');
-//         //results.push({start: values[0] end: values[1]});
-//     }
+        let start_val = values[0].parse::<i32>().unwrap();
+        let end_val =  values[1].parse::<i32>().unwrap();
 
-// }
+        results.push(Range{start: start_val, end: end_val});
+    }
+    
+    return results;
+}
 
 fn get_input()-> Result<String, Error>{
     let cur_path = env::current_dir()?;
