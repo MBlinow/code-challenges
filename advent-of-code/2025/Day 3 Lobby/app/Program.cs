47b4af2cb9c13977b1a7521a92150624ae4e37a5
MattBlinow <mblinow@gmail.com>
2026-01-05 20:09:48 -0500

    day 3 part 1 complete

--- advent-of-code/2025/Day 3 Lobby/app/Program.cs
+++ advent-of-code/2025/Day 3 Lobby/app/Program.cs
@@ -6,6 +6,8 @@ var input_lines = parse_input();
 
 Console.WriteLine($"Input lines: {input_lines.Count}");
 
+Console.WriteLine($"Jolts: {get_jolts(input_lines)}");
+
 static List<string> parse_input()
 {
     StreamReader reader = new StreamReader("E:\\DEV\\code-challenges\\advent-of-code\\2025\\Day 3 Lobby\\app\\input.txt");
@@ -27,8 +29,45 @@ static int get_jolts(List<string> lines)
 
     foreach (var line in lines)
     {
-        // Process each line to calculate jolts
+        jolts +=get_jolts_from_line(line);
     }
 
     return jolts;
+}
+
+static int get_jolts_from_line(string line)
+{
+    char max_Tens = '0';
+    int max_tens_index = -1;
+
+    char max_Ones = '0';
+    int max_ones_index = -1;
+
+    // Get max tens place digit
+    for (int i = 0; i < line.Length; i++)
+    {
+        char current = line[i];
+
+        if (current > max_Ones)
+        {
+            max_Ones = current;
+            max_ones_index = i;
+        }
+        
+        bool last_char = (i == line.Length - 1);
+        if ( !last_char && current > max_Tens)
+        {
+            max_Tens = current;
+            max_tens_index = i;
+
+            // reset ones when setting tens
+            max_Ones = '0';
+            max_ones_index = -1;
+        }
+        
+        
+    }
+
+    string max_val_str = max_Tens.ToString() + max_Ones.ToString();
+    return int.Parse(max_val_str);
 }
