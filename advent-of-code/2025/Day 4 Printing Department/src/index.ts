3ab9057147e39bcfb7a49c7bdd50038f24c2ae64
MattBlinow <mblinow@gmail.com>
2026-04-12 19:05:12 -0400

    add debug logging and some fixes
    still seeing one extra column and cell tests passing too frequently

--- advent-of-code/2025/Day 4 Printing Department/src/index.ts
+++ advent-of-code/2025/Day 4 Printing Department/src/index.ts
@@ -7,6 +7,9 @@ import { fileURLToPath } from 'node:url';
 
 type Grid = string[][];
 
+const TARGET_CHAR = "@";
+const INPUT_FILENAME = 'inputTest';
+
 const main = () => {
     const grid = getData();
     const result = scanGrid(grid);
@@ -22,30 +25,51 @@ const scanGrid = (grid: Grid): number  => {
     const gridWidth = grid[0].length;
     const gridHeight = grid.length; 
 
-     let totalValid = 0;
+    let totalValid = 0;
     let py = 0;
     let px = 0;
 
-    while ( px < gridWidth && py < gridHeight ){
-        let curChar = grid[py][px] ?? undefined;
-        if (grid[py][px] === ".") continue;
-        
-        const isValid = pointIsValid(px, py, grid);
-        if (isValid) totalValid++;
+    let debugBuffer = '';
+
+    let totalChars = gridWidth * gridHeight;
+    for ( let i = 0; i < totalChars; i++ ){
+        let curRow = grid[py];
+        let curChar: string = 'F';
+
+        // Handle non-@ chars
+        if (curRow){
+            curChar = curRow[px] ?? '';
+        }
+
+        if (curChar === "." || curChar === "F") {
+            debugBuffer += curChar;
+        }
+        else {
+            const isValid = pointIsValid(px, py, grid);
+            if (isValid) totalValid++;
+
+            if (isValid) debugBuffer += 'x'
+            else debugBuffer += curChar;
+        }
 
         if (px < gridWidth) px++;
         else {
+            console.log(`${debugBuffer}`)
             py++;
             px = 0;
+
+            debugBuffer = '';
         } 
     }
-    return 0;
+
+    return totalValid;
 }
+
+//4046 too high
 
 const pointIsValid = (x: number, y: number, grid: Grid): boolean => {
     let collisions = 0;
 
-    
     //get three rows, 3 characters width around target
     //append three rows (9 char total) 
     //count rollChars in collection.  
@@ -65,7 +89,7 @@ const pointIsValid = (x: number, y: number, grid: Grid): boolean => {
 }
 
 const countRowChunk = (startIndex: number, gridRow: string[] | undefined): number => {
-    const rollChar = '@';
+    const rollChar = TARGET_CHAR;
     let charCount = 0;
 
     if (!gridRow) return 0;
@@ -93,7 +117,8 @@ const getData = (): Grid => {
 const gridFromLines = (lines: string[]): Grid => {
     const twoDGrid: string[][] = [];
     lines.forEach(line => {
-        const lineArray = line.split('');
+        let trimmedLine = line.replace("\r", "");
+        const lineArray = trimmedLine.split('');
         twoDGrid.push(lineArray);
     });
 
@@ -102,7 +127,7 @@ const gridFromLines = (lines: string[]): Grid => {
 
 const getFileContents = () => {
     const __dirname = dirname(fileURLToPath(import.meta.url));
-    const filePath = path.join(__dirname, 'input');
+    const filePath = path.join(__dirname, INPUT_FILENAME);
     const fileContents = fs.readFileSync(filePath, 'utf8');
 
     return fileContents;
